d289d777f3676f78dddc200f286230241ba0ad70
HisShoes <ryan999000999@hotmail.co.uk>
2017-04-17 19:19:23 +0100

    Basic functionality complete, styling started
    JS, HTML, CSS, jQuery
    
    At the moment just a basic todo list app, functionally working and
    started making it look nice. Still to do some styling and allow for a
    list within a list kind of structure to group todos

---
+++ assets/js/todos.js
@@ -0,0 +1,36 @@
+
+
+//set up
+function init() {
+	createListeners();
+}
+
+//create listeners
+function createListeners() {
+	//listener for the todos on the list
+	//toggle the class for grey color and strikethrough
+	$('ul').on('click', 'li', function() {
+		$(this).toggleClass('completed');
+	});
+
+	//click on the X to delete
+	$('ul').on('click', 'span', function(event) {
+		$(this).parent().slideUp(300, function() {
+			$(this).remove();
+		});
+		// $(this).parent().remove();
+		event.stopPropagation();
+	});
+
+	//listener on inputs to add todos by pressing enter
+	$("input[type='text']").on('keypress', function(event) {
+		if(event.which === 13) {
+			var ToDoText = $(this).val();
+			$(this).val("");
+
+			$('ul').append("<li><span>X</span> " +  ToDoText + "</li>");
+		}
+	});
+}
+
+init();
