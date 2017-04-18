8eb9ed194e88e010de90477c9dd37278b4225089
HisShoes <ryan999000999@hotmail.co.uk>
2017-04-18 08:06:12 +0100

    multiple lists working
    added functionality for multiple lists working

--- Multi Lists/assets/js/todos.js
+++ Multi Lists/assets/js/todos.js
@@ -1,4 +1,4 @@
-
+var currentHidden = false;
 
 //set up
 function init() {
@@ -7,18 +7,46 @@ function init() {
 
 //create listeners
 function createListeners() {
-	//listener for the todos on the list
-	//toggle the class for grey color and strikethrough
+	//listener on the top level input to create new category - will not change
+	$(".top").on('keypress', function(event) {
+		if(event.which === 13) {
+			var ToDoText = $(this).val();
+			$(this).val("");
+
+			$(this).parent().parent().append("<div class='list outlined'><h2>"+ ToDoText + " <i class='fa fa-plus' aria-hidden='true'></i></i></h2><ul><li><input class='sub' type='text' name='' maxlength='20' placeholder='Add New Todo'></li></ul></div>");
+			reset();
+		}
+	});
+
+	$('.fa-arrows-v').on('click', function() {
+
+		if(currentHidden == false) {
+		    $('ul').slideUp();
+		}
+		else {
+			$('ul').slideDown();
+		}
+		currentHidden = !currentHidden;
+	});
+
+	dynamicListeners();
+}
+
+//listeners that may need to be created again
+function dynamicListeners() {
+	//toggle the class for todo completion
 	$('ul').on('click', 'li', function() {
 		$(this).toggleClass('completed');
 	});
 
-	//click on the X to delete
-	$('ul').on('click', 'span', function(event) {
-		$(this).parent().slideUp(300, function() {
-			$(this).remove();
-		});
-		// $(this).parent().remove();
+	//toggle the class for top level todo completion
+	$('h2').on('click', function() {
+		$(this).toggleClass('completed');
+	});
+
+	//slide toggle for a list (Should be present on 'top level' items)
+	$("h2").on('click', ".fa-plus", function(event){
+		$(this).parent().parent().find('ul').slideToggle();
 		event.stopPropagation();
 	});
 
@@ -27,27 +55,25 @@ function createListeners() {
 		if(event.which === 13) {
 			var ToDoText = $(this).val();
 			$(this).val("");
-
 			$(this).parent().parent().append("<li><span><i class='fa fa-trash'></i></span> " +  ToDoText + "</li>");
 		}
 	});
 
-	//listener on the top level input to create new category
-	$(".top").on('keypress', function(event) {
-		if(event.which === 13) {
-			var ToDoText = $(this).val();
-			$(this).val("");
-
-			$(this).parent().parent().append("<div class='list outlined'><h2>"+ ToDoText + " <i class='fa fa-plus' aria-hidden='true'></i></i></h2><ul><li><input class='sub' type='text' name='' placeholder='Add New Todo'></li></ul></div>");
-		}
+	//click on the span to delete
+	$('ul').on('click', 'span', function(event) {
+		$(this).parent().slideUp(300, function() {
+			$(this).remove();
+		});
+		event.stopPropagation();
 	});
+}
 
-	//slide toggle for a list (Should be present on 'top level' items)
-	$("h2").on('click', ".fa-plus", function(){
-		$('ul').slideToggle();
-	});
-
-
+//unbind then recreate listeners
+function reset() {
+	$('ul').unbind();
+	$('.sub').unbind();
+	$('h2').unbind();
+	dynamicListeners();
 }
 
 init();
